5b4886b1e2e081c157f4d4554d9b8616ee4088fd
FBagueSampson <79225236+FBagueSampson@users.noreply.github.com>
2024-03-22 13:03:35 -0700

    Update __init__.py
    corrected issues with calculations created from 2 changes ago.

--- oTree_Ball_Tasks/ball_task_basic/__init__.py
+++ oTree_Ball_Tasks/ball_task_basic/__init__.py
@@ -24,13 +24,13 @@ class C(BaseConstants):
 # ~~~ Primary settings
     NAME_IN_URL = 'ball_drop_task'
     PLAYERS_PER_GROUP = None # default: app does not use groups. adjust if the dictator task uses other players
-    NUM_BALLS = 10 # the total number of balls to allocate between the buckets
+    NUM_BALLS = 12 # the total number of balls to allocate between the buckets
     NUM_ROUNDS = NUM_BALLS # default: number of balls determines number of rounds
-    TASK_IS_TIMED = True # if use a hard-timer = True; if no timer = False
+    TASK_IS_TIMED = False # if use a hard-timer = True; if no timer = False
 # ~~~ Secondary (time) settings
     # default: these constants required even if TASK_IS_TIMED = False
-    TIME_FOR_TASK = 1 # length of time to complete ball task (hard timer)
-    TIMED_IN_MINUTES = True # units for TIME_FOR_TASK: minutes = True; seconds = False
+    TIME_FOR_TASK = 30 # length of time to complete ball task (hard timer)
+    TIMED_IN_MINUTES = False # units for TIME_FOR_TASK: minutes = True; seconds = False
     DISPLAY_TIME_CUTOFF = 1.5 # measured in seconds
     STAMP_DIGITS = 3 # see the function "make_it_base_sixty(..., STAMP_DIGITS)" for usage
  # ~~~ Payoffs
@@ -67,19 +67,19 @@ class Player(BasePlayer):
     time_placed_in_bucket = models.StringField() # for each ball, records time elapsed since landing on the bucket drop page -- STRINGs!
 # ~~~ Decisions during Ball Task
     bucket = models.StringField()
-    bucket_01_clicks = models.IntegerField(initial=0) # Records a 1 if the ball is placed in bucket 01, otherwise 0 (chosen)
-    bucket_02_clicks = models.IntegerField(initial=0) # Records a 1 if the ball is placed in bucket 02, otherwise 0(chosen)
+    bucket_drop_01 = models.IntegerField(initial=0) # Records a 1 if the ball is placed in bucket 01, otherwise 0 (chosen)
+    bucket_drop_02 = models.IntegerField(initial=0) # Records a 1 if the ball is placed in bucket 02, otherwise 0(chosen)
 # ~~~ Outcomes
-    sum_bucket_01_clicks = models.IntegerField(min=0) # records the sum of bucket 01 drops for each participant (computed)
-    sum_bucket_02_clicks = models.IntegerField(min=0) # records the sum of bucket 02 drops for each participant (computed)
-    donation = models.CurrencyField(min=0) # records the value of the donation in every round (computed)
+    sum_drops_bucket_01 = models.IntegerField(min=0) # records the sum of bucket 01 drops for each participant (computed)
+    sum_drops_bucket_02 = models.IntegerField(min=0) # records the sum of bucket 02 drops for each participant (computed)
+    donation = models.CurrencyField(min=0, initial=0) # records the value of the donation in every round (computed)
     total_donation = models.CurrencyField(min=0) # computes the cumulative total donated (computed)
 
 
 
 # =========== FUNCTIONS =========== #
 
-# ==== Tags players with their assigned treatment conditions from subsession 1 ==== #
+# ~~~ Tags players with their assigned treatment conditions from subsession 1 ~~~ #
 def import_treatments(subsession: Subsession):
     for p in subsession.get_players():
         participant = p.participant
@@ -124,7 +124,7 @@ def creating_session(subsession: Subsession):
     else: # in every subsession, rewrite participant characteristics so that they're constant across rounds
         import_treatments(subsession)
 
-# ~~~ Returns the name of the charity conditional on treatment condition  ##########***************************************
+# ~~~ Returns the name of the charity conditional on treatment condition
 def set_charity_name(player: Player):
     if player.participant.treatments["task"] == False:
         charity_name = C.CHARITY_TITLE
@@ -132,68 +132,21 @@ def set_charity_name(player: Player):
         charity_name = 'NA'
     return charity_name
 
-
-# ~~~ Computes the number of balls that have been allocated to each bucket
-def n_balls_in_bucket_01(player: Player):
-    return sum(p.bucket_01_clicks for p in player.in_all_rounds())
-
-def n_balls_in_bucket_02(player: Player):
-    return sum(p.bucket_02_clicks for p in player.in_all_rounds())
-
-# ~~~ Cumulative sum of balls placed into each bucket
-def compute_balls_per_urn(player: Player):
-    player.sum_bucket_01_clicks = n_balls_in_bucket_01(player)
-    player.sum_bucket_02_clicks = n_balls_in_bucket_02(player)
-
-# ~~~ Set participant payoffs for each so they get paid
-def set_payoff(player: Player):
-    if player.participant.treatments["task"] == False: # player.participant.treatments["task"] # player.task == False
-        player.payoff = cu(player.bucket_02_clicks*C.PAYOFF_DICTATOR/100) # if player is dictator task, paid for balls placed in bucket_02 only
-    else:
-        player.payoff = cu(player.bucket_01_clicks*C.PAYOFF_BUCKET_01/100 + player.bucket_02_clicks*C.PAYOFF_BUCKET_02/100) # if player is rule-following task, payoff for balls placed into either bucket
-
-# ~~~ Record player donations to charity
-def set_donation(player: Player):
-    if player.participant.treatments["task"] == False: # player.task == False:
-        donation = cu((player.bucket_01_clicks*C.PAYOFF_DICTATOR)/100)
-    else:
-        donation = cu(0)
-    return donation
-
-# ~~~ Record total player donations to charity
-def set_total_donation(player: Player):
-    return sum(p.donation for p in player.in_all_rounds())
-
-
 # ~~~ Record app results for posterity
 def package_payoff_object(player: Player):
     player.participant.paid_rounds_object = {'payoff_ball_task': sum(p.payoff for p in player.in_all_rounds()),
                                              'bucket_01_allocations': n_balls_in_bucket_01(player),
                                              'bucket_02_allocations': n_balls_in_bucket_02(player),
                                              'charity': set_charity_name(player), 
-                                             'donation': set_total_donation(player),}
+                                             'donation': cu(set_total_donation(player)),}
 
 # ~~~ Get hard-time remaining in ball-task
 def get_timeout_seconds(player: Player):
     import time
     return player.participant.expiry - time.time()
 
-# ~~~ Records the bucket selected by participant
-def set_bucket_selection(player: Player, timeout_happened):
-    if timeout_happened == True:
-        player.bucket = None
-        player.bucket_01_clicks = 0
-        player.bucket_02_clicks = 0
-    else:
-        if player.bucket == 'bucket_01':
-            player.bucket_01_clicks = 1 # += 1
-            player.bucket_02_clicks = 0 ## adjustments for server issues
-        else:
-            player.bucket_01_clicks = 0 ## adjustments for server issues
-            player.bucket_02_clicks = 1 # += 1
-
-
-def get_page_vars(player: Player):  ##########***************************************
+# ~~~ Grabs data for templates; reduces redundancies ~~~
+def get_page_vars(player: Player):
     if player.participant.treatments["task"] == False:
         target_title = set_charity_name(player)
         non_target_history_title = 'Your'
@@ -217,20 +170,85 @@ def get_page_vars(player: Player):  ##########**********************************
             non_target_history_title = non_target_title,
         )
 
+def summarise_current_state(player: Player):
+    return dict(
+        balls_in_bucket01 = n_balls_in_bucket_01(player),
+        balls_in_bucket02 = n_balls_in_bucket_02(player),
+        bucket_01_payoffs = sum_earned_bucket_01(player),
+        bucket_02_payoffs = sum_bucket_02_payoff(player),
+        total_earnings = sum_bucket_01_payoff(player) + sum_bucket_02_payoff(player),
+    )
 
+# ~~~ Set participant payoffs for each so they get paid
+def set_payoff(player: Player):
+    if player.participant.treatments["task"] == False: # player.participant.treatments["task"] # player.task == False
+        player.payoff = cu(player.bucket_drop_02*C.PAYOFF_DICTATOR/100) # if player is dictator task, paid for balls placed in bucket_02 only
+    else:
+        player.payoff = cu(player.bucket_drop_01*C.PAYOFF_BUCKET_01/100 + player.bucket_drop_02*C.PAYOFF_BUCKET_02/100) # if player is rule-following task, payoff for balls placed into either bucket
 
-# ~~~ Computes the total payoffs attributable to each bucket
-def sum_bucket_01_payoff(player: Player):  ##########***************************************
+# ~~~ Record player donations to charity
+def set_donation(player: Player):
+    if player.participant.treatments["task"] == False: # player.task == False:
+        donation = cu((player.bucket_drop_01*C.PAYOFF_DICTATOR)/100)
+    else:
+        donation = cu(0)
+    return donation
+
+# ~~~ Computes the number of balls that have been allocated to each bucket
+def n_balls_in_bucket_01(player: Player):
+    return sum(p.bucket_drop_01 for p in player.in_all_rounds())
+
+def n_balls_in_bucket_02(player: Player):
+    return sum(p.bucket_drop_02 for p in player.in_all_rounds())
+
+# ~~~ Cumulative sum of balls placed into each bucket
+def compute_balls_per_urn(player: Player):
+    player.sum_drops_bucket_01 = n_balls_in_bucket_01(player)
+    player.sum_drops_bucket_02 = n_balls_in_bucket_02(player)
+
+# ~~~ Record total player donations to charity
+def set_total_donation(player: Player):
+    if player.participant.treatments["task"] == False:
+        return (n_balls_in_bucket_01(player)*C.PAYOFF_DICTATOR)/100
+    else:
+        return 0
+
+# ~~~ Computes the total participant payoffs attributable to each bucket
+def sum_bucket_01_payoff(player: Player):
     if player.participant.treatments["task"] == False:
         return 0 
     else:
         return n_balls_in_bucket_01(player)*C.PAYOFF_BUCKET_01/100 
 
-def sum_bucket_02_payoff(player: Player):  ##########***************************************
+def sum_bucket_02_payoff(player: Player):
     if player.participant.treatments["task"] == False:
         return n_balls_in_bucket_02(player)*C.PAYOFF_DICTATOR/100 
     else:
         return n_balls_in_bucket_02(player)*C.PAYOFF_BUCKET_02/100 
+
+# ~~~ Computes the total payoff/donation value in bucket 01 for display ~~~
+def sum_earned_bucket_01(player: Player):
+    if player.participant.treatments["task"] == False:
+        return set_total_donation(player)
+    else:
+        return sum_bucket_01_payoff(player)
+
+# ~~~ Records the bucket selected by participant
+def set_bucket_selection(player: Player, timeout_happened):
+    if timeout_happened == True:
+        player.bucket = None
+        player.bucket_drop_01 = 0
+        player.bucket_drop_02 = 0
+    else:
+        if player.bucket == 'bucket_01':
+            player.bucket_drop_01 = 1 # += 1
+            player.bucket_drop_02 = 0 ## adjustments for server issues
+        else:
+            player.bucket_drop_01 = 0 ## adjustments for server issues
+            player.bucket_drop_02 = 1 # += 1
+
+
+
 
 
 # =========== PAGES =========== #
@@ -278,12 +296,10 @@ class Ball_Task(Page):
             return player.round_number < C.NUM_ROUNDS + 1
     
     def js_vars(player: Player):
-        bucket_01_num_balls = n_balls_in_bucket_01(player)
-        bucket_02_num_balls = n_balls_in_bucket_02(player)
         return dict(
                 round_number = player.round_number,
-                bucket_01_num_balls = bucket_01_num_balls,
-                bucket_02_num_balls = bucket_02_num_balls,
+                bucket_01_num_balls = n_balls_in_bucket_01(player),
+                bucket_02_num_balls = n_balls_in_bucket_02(player),
             )
 
     def before_next_page(player, timeout_happened):
@@ -303,14 +319,10 @@ class Ball_Task(Page):
             player.time_stamp = round(get_timeout_seconds(player), 4)
             player.did_timeout_happen = False
 
-
     def vars_for_template(player: Player):
-        bucket_01_allocations = n_balls_in_bucket_01(player)
-        bucket_02_allocations = n_balls_in_bucket_02(player)
-        bucket_01_payoff = sum_bucket_01_payoff(player)
-        bucket_02_payoff = sum_bucket_02_payoff(player)
     # Dictates the information visible to the player on the buckets
         page_vars = get_page_vars(player)
+        player_info = summarise_current_state(player)
         if C.TASK_IS_TIMED == True:
             row_two_contents = 'ball_bag_with_tables.html'
         else:
@@ -325,13 +337,13 @@ class Ball_Task(Page):
     # Variables sent to template
         return dict (
             player_task = player.participant.treatments["task"],
-            balls_remaining = C.NUM_BALLS - player.round_number + 1,
-            bucket_01_payoff = bucket_01_payoff,
-            bucket_02_payoff = bucket_02_payoff,
-            player_earnings = bucket_01_payoff + bucket_02_payoff,
-            total_balls = bucket_01_allocations + bucket_02_allocations, 
-            bucket_01_allocations = bucket_01_allocations,
-            bucket_02_allocations = bucket_02_allocations,
+            balls_remaining = max(C.NUM_BALLS - player.round_number + 1, 0),
+            bucket_01_payoff = player_info["bucket_01_payoffs"],
+            bucket_02_payoff = player_info["bucket_02_payoffs"],
+            player_earnings = player_info["total_earnings"],
+            bucket_01_allocations = player_info["balls_in_bucket01"],
+            bucket_02_allocations = player_info["balls_in_bucket02"],
+            total_balls = player_info["balls_in_bucket01"] + player_info["balls_in_bucket02"], 
             bucket_01_title = page_vars["target_title"],
             bucket_02_title = page_vars["non_target_title"],
             bucket_01_historyName = page_vars["target_history_title"],
@@ -353,24 +365,21 @@ class Results(Page):
 
     def vars_for_template(player: Player):
         page_vars = get_page_vars(player)
-        bucket_01_allocations = n_balls_in_bucket_01(player)
-        bucket_02_allocations = n_balls_in_bucket_02(player)
-        bucket_01_payoff = sum_bucket_01_payoff(player)
-        bucket_02_payoff = sum_bucket_02_payoff(player)
+        player_info = summarise_current_state(player)
         if player.task == False:
             player_task = 'Dictator'
         else:
             player_task = 'Rule-Following'
         return dict(
             player_task = player_task,
-            player_earnings = player.participant.payoff, 
-            bucket_01_payoff = cu(bucket_01_payoff),
-            bucket_02_payoff = cu(bucket_02_payoff),
-            bucket_01_title = page_vars["target_title"],
-            bucket_02_title = page_vars["non_target_title"],
-            bucket_01_allocations = bucket_01_allocations,
-            bucket_02_allocations = bucket_02_allocations,
-            total_balls = bucket_01_allocations + bucket_02_allocations,
+            bucket_01_payoff = cu(player_info["bucket_01_payoffs"]),
+            bucket_02_payoff = cu(player_info["bucket_02_payoffs"]),
+            player_earnings = cu(player_info["total_earnings"]),
+            bucket_01_title = page_vars["target_possessive"],
+            bucket_02_title = page_vars["non_target_possessive"],
+            bucket_01_allocations = player_info["balls_in_bucket01"],
+            bucket_02_allocations = player_info["balls_in_bucket02"],
+            total_balls = player_info["balls_in_bucket01"] + player_info["balls_in_bucket02"],
         )
 
 
